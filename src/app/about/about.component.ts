15eb1539d5551cf4d58e65fb2feab0c42dd9f90b
Wendell Wayne Estrada <wendell.estrada@irely.com>
2017-05-03 18:17:44 +0800

    update about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,12 +2,62 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'wfe-about',
+    styles: [
+        `
+        img {
+            border-top-left-radius: 50% 50%;
+            border-top-right-radius: 50% 50%;
+            border-bottom-right-radius: 50% 50%;
+            border-bottom-left-radius: 50% 50%;
+        }
+        `
+    ],
     template:
     `
-    <h1>About Page</h1>
-    <p>
-    State management within Angular started out as a single celled organism if you will in the form of a single controller managing all the state for the application. If this is a single page application, one controller makes sense right? We emerged out of the ice age by starting to group our views and controllers into smaller, self-contained units either within a directive or a route. This was a vast improvement, but there was still the problem of managing complex state within our applications. It was not uncommon for us to have bits and pieces of state strewn across our application tucked inside of controllers, services, routes, directives, and occasionally, in our templates. Mutable state in itself is not inherently evil but shared mutable state is a recipe for disaster.
-    </p>
+    <h1>About codeblocks</h1>
+    <br>
+    <div class="row flex-items-sm-center">
+        <div class="col-sm-4" style="text-align: center">
+            <h1 style="color: #483654">
+                <span>
+                    <clr-icon size=240 shape="blocks-group"></clr-icon>
+                     kodeblocks
+                </span>
+            </h1>
+        </div>
+        <div class="col-sm-6">
+            <p><b>kodeblocks</b> - We came up with the name kodeblocks based on a concept of modular programming paradigm which is a technique of developing software by separating the functionality of a program into small and independent modules that are merged into one to get into the final working software. In the business side, this forces us to think more deeply about how we provide solutions to a huge business requirements by dividing and separating them into its smaller constituent components. Thus, we can design clear, simple, and independent solutions that are combined together to get the final requirements done in a shortest possible time.</p>
+        </div>
+
+        <div class="col-sm-6">
+            <p><b>kodeblocks</b> - We came up with the name kodeblocks based on a concept of modular programming paradigm which is a technique of developing software by separating the functionality of a program into small and independent modules that are merged into one to get into the final working software. In the business side, this forces us to think more deeply about how we provide solutions to a huge business requirements by dividing and separating them into its smaller constituent components. Thus, we can design clear, simple, and independent solutions that are combined together to get the final requirements done in a shortest possible time.</p>
+        </div>
+        <div class="col-sm-4" style="text-align: center">
+            <h1 style="color: #630E8E">
+                <span>
+                    <clr-icon size=240 shape="vm"></clr-icon>
+                     kodeblocks
+                </span>
+            </h1>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="row flex-items-large-center">
+        <div class="col-sm-4" style="text-align: center">
+            <img src="https://en.gravatar.com/userimage/109068507/ec51735f1bcf55ec980df6c4dd54d841.jpg" alt="image">
+            <p>Wendell  started and continues to lead the kodeblocks project. He is involved in all stages of kodeblocks development. He loves engaging with designers and developers to understand their use cases and help them build solutions</p>
+        </div>
+        <div class="col-sm-4" style="text-align: center">
+            <img src="https://en.gravatar.com/userimage/109068507/ec51735f1bcf55ec980df6c4dd54d841.jpg" alt="image">
+            <p>Wayne contributes to the look and feel of kodeblocks and works on the code. Scott's our ace researcher and storyteller. He has captured the stories of why we did what we did and the assumptions we've made.</p>
+        </div>
+        <div class="col-sm-4" style="text-align: center">
+            <img src="https://en.gravatar.com/userimage/109068507/ec51735f1bcf55ec980df6c4dd54d841.jpg" alt="image">
+            <p>Estrada is the technical lead for kodeblocks. He spends most of his day working on the overall architecture of wafe as well as building Angular components for kodeblocks.</p>
+        </div>
+    </div>
     `
 })
 export class AboutComponent {}
--- src/app/getting-started/getting-started.component.ts
+++ src/app/getting-started/getting-started.component.ts
@@ -10,14 +10,26 @@ import { Component } from "@angular/core";
     template:
     `
     <h1>Getting Started</h1>
-    <div class="row flex-items-xs-center">
-        <div class="col-sm-6">
-            <p>webpack is a tool to build JavaScript modules in your application. To start using webpack from its cli or api, follow the Installation instructions. webpack simplifies your workflow by quickly constructing a dependency graph of your application and bundling them in the right order. webpack can be configured to customise optimisations to your code, to split vendor/css/js code for production, run a development server that hot-reloads your code without page refresh and many such cool features. Learn more about why you should use webpack.</p>
-        </div>
+    <div class="row flex-items">
         <div class="col-sm-4" style="text-align: center">
             <h1 style="color: #483654">
                 <span>
                     <clr-icon size=240 shape="blocks-group"></clr-icon>
+                     kodeblocks
+                </span>
+            </h1>
+        </div>
+        <div class="col-sm-6">
+            <p><b>kodeblocks</b> - We came up with the name kodeblocks based on a concept of modular programming paradigm which is a technique of developing software by separating the functionality of a program into small and independent modules that are merged into one to get into the final working software. In the business side, this forces us to think more deeply about how we provide solutions to a huge business requirements by dividing and separating them into its smaller constituent components. Thus, we can design clear, simple, and independent solutions that are combined together to get the final requirements done in a shortest possible time.</p>
+        </div>
+
+        <div class="col-sm-6">
+            <p><b>kodeblocks</b> - We came up with the name kodeblocks based on a concept of modular programming paradigm which is a technique of developing software by separating the functionality of a program into small and independent modules that are merged into one to get into the final working software. In the business side, this forces us to think more deeply about how we provide solutions to a huge business requirements by dividing and separating them into its smaller constituent components. Thus, we can design clear, simple, and independent solutions that are combined together to get the final requirements done in a shortest possible time.</p>
+        </div>
+        <div class="col-sm-4" style="text-align: center">
+            <h1 style="color: #630E8E">
+                <span>
+                    <clr-icon size=240 shape="vm"></clr-icon>
                      kodeblocks
                 </span>
             </h1>
